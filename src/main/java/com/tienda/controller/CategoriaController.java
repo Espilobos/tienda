a6a4303d57958542bc171b81046bfded63fdd65e
Sebas <sebastian.espilobos@gmail.com>
2026-02-02 11:20:16 -0600

    Semana 3

---
+++ src/main/java/com/tienda/controller/CategoriaController.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda.controller;
+
+import com.tienda.service.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author sebas
+ */
+@Controller
+@RequestMapping("/Categoria")
+public class CategoriaController {
+    @Autowired
+    private CategoriaService categoriaService;
+    
+    @GetMapping("/listado")
+    public String Inicio(Model model){
+        var categorias = categoriaService.getCategorias(false);
+        model.addAttribute("categorias", categorias);
+        model.addAttribute("categorias", categorias.size());
+        return "/categoria/listado";
+    }
+}
