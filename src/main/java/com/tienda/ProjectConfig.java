cd06002288bb72153cda3f8c65543704b64cd498
Sebas <sebastian.espilobos@gmail.com>
2026-03-24 22:42:48 -0600

    11.Nuevo Login 2

--- src/main/java/com/tienda/ProjectConfig.java
+++ src/main/java/com/tienda/ProjectConfig.java
@@ -103,65 +103,12 @@ public class ProjectConfig implements WebMvcConfigurer {
         "/facturar/carrito"
     };
 
-    @Autowired
-    public void configurerGlobal(AuthenticationManagerBuilder build,
-            @Lazy PasswordEncoder passwordEncoder,
-            @Lazy UserDetailsService userDetailsService) throws Exception {
-        build.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
-    }
-    
-    @Autowired
-    private RutaService rutaService;
-
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        var rutas = rutaService.getRutas();
-        
-        http.authorizeHttpRequests(requests -> {
-            for (Ruta ruta : rutas) {
-                if (ruta.isRequiereRol()) {
-                    requests.requestMatchers(ruta.getRuta()).hasRole(ruta.getRol().getRol());
-                } else {
-                    requests.requestMatchers(ruta.getRuta()).permitAll();
-                }
-            }
-            requests.anyRequest().authenticated();
-        });
-        http.authorizeHttpRequests(request -> request
-                .requestMatchers(PUBLIC_URLS).permitAll()
-                .requestMatchers(ADMIN_URLS).hasRole("ADMIN")
-                .requestMatchers(ADMIN_OR_VENDEDOR_URLS).hasAnyRole("ADMIN", "VENDEDOR")
-                .requestMatchers(USUARIO_URLS).hasRole("USUARIO")
-                .anyRequest().authenticated()
-        ).formLogin(form -> form // Configuración de formulario de login
-                .loginPage("/login")
-                .loginProcessingUrl("/login")
-                .defaultSuccessUrl("/", true)
-                .failureUrl("/login?error=true")
-                .permitAll()
-        ).logout(logout -> logout // Configuración de logout
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/login?logout=true")
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID")
-                .permitAll()
-        ).exceptionHandling(exceptions -> exceptions // Manejo de excepciones
-                .accessDeniedPage("/acceso_denegado")
-        ).sessionManagement(session -> session // Configuración de sesiones
-                .maximumSessions(1)
-                .maxSessionsPreventsLogin(false)
-        );
-        return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
-    
-}
-//
-//    //Este método será reemplazado la siguiente semana
+        
+    //Este método será reemplazado la siguiente semana
 //    @Bean
 //    public UserDetailsService users(PasswordEncoder passwordEncoder) {
 //        UserDetails admin = User.builder()
@@ -184,4 +131,50 @@ public class ProjectConfig implements WebMvcConfigurer {
 //
 //        return new InMemoryUserDetailsManager(admin, sales, user);
 //    }
-//}
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder build,
+            @Lazy PasswordEncoder passwordEncoder,
+            @Lazy UserDetailsService userDetailsService) throws Exception {
+        build.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    }
+    
+    @Autowired
+    private RutaService rutaService;
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        var rutas = rutaService.getRutas();
+        http.authorizeHttpRequests(requests -> {
+            for (Ruta ruta : rutas) {
+                if (ruta.isRequiereRol()) {
+                    requests.requestMatchers(ruta.getRuta()).hasRole(ruta.getRol().getRol());
+                } else {
+                    requests.requestMatchers(ruta.getRuta()).permitAll();
+                }
+            }
+            requests.anyRequest().authenticated();
+        });
+        http.formLogin(form -> form
+                .loginPage("/login")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/", true)
+                .failureUrl("/login?error=true")
+                .permitAll()
+        ).logout(logout -> logout
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login?logout=true")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .permitAll()
+        ).exceptionHandling(exceptions -> exceptions
+                .accessDeniedPage("/acceso_denegado")
+        ).sessionManagement(session -> session
+                .maximumSessions(1)
+                .maxSessionsPreventsLogin(false)
+        );
+        return http.build();
+    }
+}
+
+
--- src/main/java/com/tienda/repository/UsuarioRepository.java
+++ src/main/java/com/tienda/repository/UsuarioRepository.java
@@ -1,11 +1,22 @@
 package com.tienda.repository;
 
 import com.tienda.domain.Usuario;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
     
-    Optional<Usuario> findByUsernameAndActivoTrue(String username);
+    public Optional<Usuario> findByUsernameAndActivoTrue(String username);
+
+    public List<Usuario> findByActivoTrue();
+    
+    public Optional<Usuario> findByUsername(String username);
+    
+    public Optional<Usuario> findByUsernameOrCorreo(String username, String correo);
+    
+    public Optional<Usuario> findByUsernameAndPassword(String username, String password);
+    
+    public boolean existsByUsernameOrCorreo(String username, String correo);
     
 }
